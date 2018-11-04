98b21f14f9ab215f1aae43c37f73a71aa538720c
Theja <lakshmi.panjam@netcracker.com>
2018-11-04 11:36:02 +0530

    Push code

---
+++ src/main/java/com/test/hibernateProj/HibernateProjApplication.java
@@ -0,0 +1,12 @@
+package com.test.hibernateProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateProjApplication.class, args);
+	}
+}
